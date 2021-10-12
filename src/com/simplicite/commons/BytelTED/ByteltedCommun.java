78364272aa77d97fd5f39525d4bbf4a446575458
designer (Designer) <no-reply@simplicite.fr>
2021-10-12 11:20:51 +0200

    Initial commit

---
+++ src/com/simplicite/commons/BytelTED/ByteltedCommun.java
@@ -0,0 +1,7 @@
+package com.simplicite.commons.BytelTED;
+
+import com.simplicite.util.ObjectDB;
+
+public class ByteltedCommun extends ObjectDB {
+	private static final long serialVersionUID = 1L;
+}
