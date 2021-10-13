c8e6cd2b74069a8a1703f447144cacf53d23ba56
designer (Designer) <no-reply@simplicite.fr>
2021-10-13 16:22:23 +0200

    Milestone

--- resources/ObjectInternal/ByteltedCommunication/SCRIPT.js
+++ resources/ObjectInternal/ByteltedCommunication/SCRIPT.js
@@ -3,21 +3,76 @@
 const ByteltedCommunication = (function(ui) {
 	const app = ui.getAjax();
 	let obj;
-	const $vars = $('<div/>').addClass("previsu-vars");
-	const $template = $('<div/>').addClass("previsu-template");
-	
-	function load() {
-		$vars.html('<div><strong>TODO</strong>: Variables</div>');
+	let html;
 
-		const doc = obj.getField('byteltedComContenu').value();
-		$template.html($('<iframe/>').attr('src', app.documentURL(doc.object, doc.field, doc.rowid, doc.id, 'inline')));
+	let $form, $vars, $content, $template;
+
+	function write(d) {
+		let doc = $content.contents()[0];
+		doc.open();
+		doc.write(d);
+		doc.close();
+	}
+
+	function load() {
+		try {
+			const vars = obj.getField('byteltedComVariables').value();
+			if (vars.variables) {
+				for (let i = 0; i < vars.variables.length; i++) {
+					let v = vars.variables[i];
+					$form.append($('<input class="form-control"/>')
+						.val(v.value)
+						.attr('type', v.type)
+						.attr('id', 'previsu-var-' + v.name)
+						.attr('name', v.name)
+						.attr('placeholder', v.label));
+				}
+			} else
+				$form.text('Aucune variable définie');
+	
+			const doc = obj.getField('byteltedComContenu').value();
+			$.ajax({
+				url: app.documentURL(doc.object, doc.field, doc.rowid, doc.id, 'inline')
+			}).then(function(h) {
+				html = h;
+				write(html);
+				if (vars.variables)
+					$vars.append($('<button class="btn btn-secondary"/>').append("Tester").click(test));
+			});
+		} catch (e) {
+			console.error(e);
+			$vars.html('<div><strong>Erreur</strong>: ' + (e.message || e) + '</div>');
+		}
+	}
+	
+	function test() {
+		// TODO: remplacer par ui.loadMustache() lorsque disponible
+		ui.loadScript({
+			url: '/scripts/mustache/mustache.min.js',
+			onload: function() {
+				const a = $form.serializeArray();
+				var data = {};
+				$.map(a, function(n) {
+        			data[n['name']] = n['value'];
+				 });
+				 console.log(data);
+				write(Mustache.render(html, data));
+			}
+		});
 	}
 
 	function previsu(o) {
 		obj = o;
+
+		$form = $('<form/>').attr('autocomplete', 'off');
+		$vars = $('<div/>').addClass("previsu-vars").html($form);
+	
+		$content = $('<iframe/>');
+		$template = $('<div/>').addClass("previsu-template").html($content);
+
 		ui.view.tools.dialog({
 			title: 'Prévisualisation',
-			content: $('<div id="communication-previsu"/>').append($vars).append($template),
+			content: $('<div id="communication-previsu"/>').empty().append($vars).append($template),
 			modal: true,
 			width: '90%',
 			buttons: [{ name: 'Fermer', style: 'primary', close: true }],
