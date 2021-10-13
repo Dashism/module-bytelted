1a3a4c02f2fd1d87f3de17a985e2739c4046d550
dazoulay (David AZOULAY) <dazoulay@simplicite.fr>
2021-10-13 22:41:42 +0200

    Adapter

--- src/com/simplicite/adapters/BytelTED/ByteltedMatriceHermes.java
+++ src/com/simplicite/adapters/BytelTED/ByteltedMatriceHermes.java
@@ -1,7 +1,13 @@
 package com.simplicite.adapters.BytelTED;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import org.apache.commons.lang.StringUtils;
+
 import com.simplicite.util.exceptions.PlatformException;
 import com.simplicite.util.AppLog;
+import com.simplicite.util.Tool;
 
 /**
  * Import de la matrice Hermes
@@ -9,33 +15,66 @@ import com.simplicite.util.AppLog;
 public class ByteltedMatriceHermes extends com.simplicite.util.integration.SimpleSAXParserAdapter {
 	private static final long serialVersionUID = 1L;
 
+	private boolean inToken = false;
+	private JSONArray templates = null;
+	private JSONObject template = null;
+	private JSONArray variables = null;
+
 	@Override
 	public void startProcess() throws PlatformException, InterruptedException {
-		AppLog.info("Start process", getGrant());
+		templates = new JSONArray();
 	}
 
 	@Override
 	public void startTagProcess(String uri, String localName, String qName, String path) throws PlatformException, InterruptedException {
-		AppLog.info("Start tag process " + uri + ", " + localName + ", " + qName + ", " + path, getGrant());
+		if ("token".equals(qName)) {
+			inToken = true;
+		}
+	}
+
+	private void addTemplate() {
+		if (template != null) {
+			template.put("variables", variables);
+			templates.put(template);
+		}
 	}
 
 	@Override
 	public void processAttribute(String uri, String localName, String qName, String value, String path) throws PlatformException, InterruptedException {
-		AppLog.info("Process attribute " + uri + ", " + localName + ", " + qName + ", " + path, getGrant());
+		AppLog.info(qName + " = " + value, getGrant());
+		if ("idTemplate".equals(qName)) {
+			addTemplate();
+			template = new JSONObject().put("name", value);
+			variables = new JSONArray();
+		} else if (inToken && "id".equals(qName)) {
+			if (!Tool.isEmpty(value)) {
+				variables.put(new JSONObject()
+					.put("name", value)
+					.put("type", "text")
+					.put("label", StringUtils.capitalize(value.toLowerCase()))
+				);
+			}
+			AppLog.info(templates.toString(2), getGrant());
+		}
 	}
 
 	@Override
 	public void processValue(String value) throws PlatformException, InterruptedException {
-		AppLog.info("Process value " + value, getGrant());
+		// Non utilisé
 	}
 
 	@Override
 	public void endTagProcess(String uri, String localName, String qName, String path) throws PlatformException, InterruptedException {
-		AppLog.info("End tag process " + uri + ", " + localName + ", " + qName + ", " + path, getGrant());
+		if ("token".equals(qName)) {
+			inToken = false;
+		}
 	}
 
 	@Override
 	public void endProcess() throws PlatformException, InterruptedException {
-		AppLog.info("End process", getGrant());
+		addTemplate();
+		
+		// TODO: mise à jour des variables des communications en matchant sur l'ID template
+		AppLog.info(templates.toString(2), getGrant());
 	}
 }
